ba8a8c3ce63b39c74e14a83e215ba55e85082f27
Dennis Diaz <Diaz3737@gmail.com>
2022-08-26 21:48:35 -0400

    Added Navbar and Home.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+import Home from './Home';
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+        <img className='logo' src='#' />
+        <Navbar />
+        <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul className='nav-list'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
