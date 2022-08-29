da90255ec39dec177b8cec526afa57f39f8215fa
Dennis Diaz <Diaz3737@gmail.com>
2022-08-28 23:23:46 -0400

    Started navbar styling.

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='home-page'>
-        <h1>Welcome!</h1>
+        <h1>Hello! I'm Dennis!</h1>
+        <p>I'm a self taught web developer looking to break into the tech field. I created the website to highlight my skills. Please feel free to look around!</p>
     </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-        <ul className='nav-list'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul>
-    </div>
+        <div className='navbar'>
+            <button>Home</button>
+            <button>About</button>
+            <button>Portfolio</button>
+            <button>Contact</button>
+        </div>
   )
 }
