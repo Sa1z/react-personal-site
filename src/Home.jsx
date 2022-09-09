653429b7c29c6a84104203f6b9917bf75a9cbb5a
Dennis Diaz <Diaz3737@gmail.com>
2022-09-08 21:38:58 -0400

    Added Tailwindcss functionality.

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='home-page'>
-        <h1>Hello! I'm Dennis!</h1>
+        <h1 className='text-3xl font-bold underline'>Hello! I'm Dennis!</h1>
         <p>I'm a self taught web developer looking to break into the tech field. I created the website to highlight my skills. Please feel free to look around!</p>
     </div>
   )
