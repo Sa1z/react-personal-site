1cc6aa51d17393cb88b2c357a662e2d0d5d51840
Dennis Diaz <Diaz3737@gmail.com>
2022-09-17 14:20:09 -0400

    Added page components. Adjusted nav styling.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
+
 import './index.css';
 import Home from './Home';
 import Navbar from './Navbar';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+
 
 
 function App() {
+  const [sections, setSections] = useState({
+    home: true,
+    about: false,
+    portfolio: false,
+    contact: false
+  })
+
   return (
     <div className="App">
-        <Navbar />
-        <Home />
+        <Navbar sections={sections} />
+        {sections.home && <Home />}
+        {sections.about && <About />}
+        {sections.portfolio && <Portfolio />}
+        {sections.contact && <Contact />}
     </div>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 export default function Navbar() {
 
-  const btnStyles = 'p-4'
+  const btnStyles = 'p-4 bg-neutral-700 rounded-full hover:bg-neutral-500'
+
+  const onClick = link => {
+    
+  }
 
   return (
-        <nav>
-          <img src="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80" alt="logo" className="w-24 p-2 rounded-full" />
-          <div className='flex justify-end'>
-            <button className={btnStyles}>Home</button>
-            <button className={btnStyles}>About</button>
-            <button className={btnStyles}>Portfolio</button>
-            <button className={btnStyles}>Contact</button>
+        <nav className='relative container mx-auto p-6'>
+          <div className='flex items-center justify-between'>
+            <div className="pt-2">
+              <img src="https://images.unsplash.com/photo-1537151625747-768eb6cf92b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80" alt="logo" className="w-24 p-2 rounded-full" />
+              </div>
+              <div className="hidden md:flex space-x-6">
+                <button className={btnStyles}>Home</button>
+                <button className={btnStyles}>About</button>
+                <button className={btnStyles}>Portfolio</button>
+                <button className={btnStyles}>Contact</button>
+              </div>
           </div>
         </nav>
   )
