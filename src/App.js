2817a689053325abf591127ff2f4358b7c928395
Dennis Diaz <Diaz3737@gmail.com>
2022-09-23 16:50:45 -0400

    Added hero image.

--- src/App.js
+++ src/App.js
@@ -10,41 +10,11 @@ import Contact from './Contact';
 
 
 function App() {
-  const [sections, setSections] = useState({
-    // Home
-    home: {
-      id: 1,
-      current: true
-    },
-    // About
-    about: {
-      id: 2,
-      current: false
-    },
-    // Portfolio
-    portfolio: {
-      id: 3,
-      current: false
-    },
-    // Contact
-    contact: {
-      id: 4,
-      current: false
-    }
-  })
-
-  function changePage(section) {
-    setSections(prevSection => prevSection.map(prev => {
-      return prev.about.id === section ? 
-      {about: {current: true}} : 
-      prev
-    }))
-  }
 
   return (
     <div className="">
-        <Navbar changePage={()=> changePage()} />
-        <div className='p-2 max-w-prose mx-auto'>
+        <Navbar />
+        <div className='p-2 mx-auto'>
           <Home />
           {/* <About />
           <Portfolio />
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className=''>
-      <h1 className='p-2 text-5xl font-bold underline'>Hire me for your next project!</h1>
-      <p className='p-4'>Hello! I'm Dennis! I'm a self taught web developer looking to break into the tech field. I created the website to highlight my skills. Please feel free to look around!</p>
+    <div className='lg:flex justify-between'>
+      <div>
+        <h1 className='p-2 text-5xl font-bold max-w-2xl underline'>Hire me for your next project!</h1>
+        <p className='p-4 text-2xl max-w-3xl'>Hello! I'm Dennis! I'm a self taught web developer looking to break into the tech field. I created the website to highlight my skills. Please feel free to look around!</p>
+      </div>
+      <img className='p-2 mr-4 drop-shadow-lg max-w-2xl rounded' src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80" alt="" />
     </div>
   )
 }
